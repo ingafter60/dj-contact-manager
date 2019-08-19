78656941b3cad187a898c1a3581c68ecb4c254b9
ing <inyoman_gurnitha@yahoo.com>
2019-08-19 10:55:37 +0700

    14. Accessing Django Admin Site

--- contact/models.py
+++ contact/models.py
@@ -5,14 +5,16 @@ from django.db import models
 # Model / table name: Contact
 class Contact(models.Model):
 	# Contact table fields
-	  name 	 = models.CharField(max_length=20)
-    email  = models.EmailField(max_length=100)
-    phone  = models.IntegerField()
-    info 	 = models.CharField(max_length=30)
-    gender = models.CharField(max_length=50, choices=(
-        ('male', 'Male'),
-        ('feamle', 'Female')
-    ))
-    image  = models.ImageField(upload_to='images/', blank=True)
-    date_added 	= models.DateTimeField(auto_now_add=True)
+  name 	 = models.CharField(max_length=20)
+  email  = models.EmailField(max_length=100)
+  phone  = models.IntegerField()
+  info 	 = models.CharField(max_length=30)
+  gender = models.CharField(max_length=50, choices=(
+      ('male', 'Male'),
+      ('feamle', 'Female')
+  ))
+  image  = models.ImageField(upload_to='images/', blank=True)
+  date_added 	= models.DateTimeField(auto_now_add=True)
 
+  def __str__(self):
+  	return self.name
