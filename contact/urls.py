6d867f2228d5fd0f56506db55c2b93710b7381c3
ing <inyoman_gurnitha@yahoo.com>
2019-08-19 09:11:03 +0700

    11. Displaying a Home page in django

---
+++ contact/urls.py
@@ -0,0 +1,8 @@
+# contact/urls.py
+from django.urls import path 
+from . import views 
+
+
+urlpatterns = [
+	path('', views.home, name='home')	
+]
--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-class home(request):
+def home(request):
 
 	return render(request, 'index.html')
